bb163554ace41af77e53cc185911de2eff520739
vlasotsky <v.lasotsky@gmail.com>
2024-06-08 03:16:07 +0200

    feat: add reversed conversion from DTO into Entity

--- src/main/java/ru/aston/aston02/util/DtoMapper.java
+++ src/main/java/ru/aston/aston02/util/DtoMapper.java
@@ -2,30 +2,34 @@ package ru.aston.aston02.util;
 
 import ru.aston.aston02.model.Genre;
 import ru.aston.aston02.model.VinylDisc;
-import ru.aston.aston02.model.to.VinylDiscTo;
+import ru.aston.aston02.model.to.VinylDiscDto;
 
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class ConverterToDto {
+public class DtoMapper {
 
     public static final int SUFFICIENT_AMOUNT_FOR_COLLECTION = 3;
 
-    private ConverterToDto() {
+    private DtoMapper() {
     }
 
-    public static VinylDiscTo getSingleTo(VinylDisc disc, List<VinylDisc> discList) {
+    public static VinylDiscDto getDto(VinylDisc disc, List<VinylDisc> discList) {
         final Long numDiscsInGenre = getGenreCount(discList).get(disc.getGenre());
         boolean isFew = numDiscsInGenre < SUFFICIENT_AMOUNT_FOR_COLLECTION;
         return createTo(disc, isFew);
     }
 
-    public static List<VinylDiscTo> getTos(List<VinylDisc> discList) {
+    public static List<VinylDiscDto> getAllDtos(List<VinylDisc> discList) {
         final Map<Genre, Long> genreCount = getGenreCount(discList);
         return discList.stream()
                 .map(disc -> createTo(disc, genreCount.get(disc.getGenre()) < SUFFICIENT_AMOUNT_FOR_COLLECTION))
                 .collect(Collectors.toList());
+    }
+
+    public static VinylDisc getEntity(VinylDiscDto discDto) {
+        return new VinylDisc(discDto);
     }
 
     private static Map<Genre, Long> getGenreCount(List<VinylDisc> discList) {
@@ -33,7 +37,7 @@ public class ConverterToDto {
                 .collect(Collectors.groupingBy(VinylDisc::getGenre, Collectors.counting()));
     }
 
-    private static VinylDiscTo createTo(VinylDisc disc, boolean isFew) {
-        return new VinylDiscTo(disc.getTitle(), disc.getArtists(), disc.getSongs(), disc.getLabel(), disc.getReleaseDate(), isFew);
+    private static VinylDiscDto createTo(VinylDisc disc, boolean isFew) {
+        return new VinylDiscDto(disc.getTitle(), disc.getArtists(), disc.getSongs(), disc.getGenre(), disc.getLabel(), disc.getReleaseDate(), isFew);
     }
 }
